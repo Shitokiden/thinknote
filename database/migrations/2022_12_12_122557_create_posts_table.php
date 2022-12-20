746a41a07dad2a09d78f2aa079eee583abe6743f
Shitokiden <stkdntkm37@gmail.com>
2022-12-20 10:21:36 +0900

    マイグレーションファイル

---
+++ database/migrations/2022_12_12_122557_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',50);
+            $table->string('theme',50);
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('size_id')->constrained();
+            $table->timestamps();
+            //NULL値可能なcreated_atとupdated_atのカラムを追加
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
